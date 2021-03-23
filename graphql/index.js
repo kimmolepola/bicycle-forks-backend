d601a39a1858b4bf54de8840a4200f48941aefd3
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-03-23 22:59:08 +0200

    prepare data, query

--- graphql/index.js
+++ graphql/index.js
@@ -1,30 +1,49 @@
 const { ApolloServer, gql } = require('apollo-server-azure-functions');
-const forksStringArray = require('./forks');
 
-exports.hello = () => 'hello';
+const { forks: forkStringArray, headers: headersStringArray } = require('./forks');
+
+const headers = () => {
+  const headerObjectsArray = [];
+  const headerIds = headersStringArray[0].split(',');
+  const headerNames = headersStringArray[1].split(',');
+  const headerTypes = headersStringArray[2].split(',');
+  for (let i = 0; i < headerIds.length; i += 1) {
+    headerObjectsArray.push({ id: headerIds[i], name: headerNames[i], type: headerTypes[i] });
+  }
+  return headerObjectsArray;
+};
 
 const forks = () => {
-  const headers = forksStringArray[0].split(',');
-  const forksObjectArray = [];
-  for (i = 1; i < forksArray.length, i++){
-    forksObjectArray.push({})
+  const headerIds = headersStringArray[0].split(',');
+  const forkObjectArray = [];
+  for (let i = 0; i < forkStringArray.length; i += 1) {
+    const forkArray = forkStringArray[i].split(',');
+    const forkObject = {};
+    for (let ii = 0; ii < headerIds.length; ii += 1) {
+      forkObject[headerIds[ii]] = forkArray[ii];
+    }
+    forkObjectArray.push(forkObject);
   }
   return (
-    forksArray.map({ asdf: 2221 })
+    forkObjectArray
   );
 };
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
+    forkListHeaders: String
     allForks: String
+    hello: String
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    allForks: () => 'Hello world!',
+    forkListHeaders: () => JSON.stringify(headers()),
+    allForks: () => JSON.stringify(forks()),
+    hello: () => 'hello',
   },
 };
 
