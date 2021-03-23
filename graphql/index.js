97cd43fb304b5db36228c3ee8a03c26797b4c5a9
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-03-23 12:40:47 +0200

    prepare data, data to object array

--- graphql/index.js
+++ graphql/index.js
@@ -1,16 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server-azure-functions');
+const forksStringArray = require('./forks');
+
+exports.hello = () => 'hello';
+
+const forks = () => {
+  const headers = forksStringArray[0].split(',');
+  const forksObjectArray = [];
+  for (i = 1; i < forksArray.length, i++){
+    forksObjectArray.push({})
+  }
+  return (
+    forksArray.map({ asdf: 2221 })
+  );
+};
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    allForks: String
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    allForks: () => 'Hello world!',
   },
 };
 
